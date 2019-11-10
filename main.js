6fb3d042709d316ba4b1f93921e2fcaa756db74c
Aimekram <janina.byszewska@gmail.com>
2019-11-10 19:25:38 +0100

    Create functions - control, start and add new todo

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log('Hello World')
+ 
+const todos = [];
+let action = 0;
+
+control();
+
+// ------------start
+function start() {
+    action = parseInt(prompt('Hello, you entered a todoapp - write a number to choose action\n1 - add new todo'), 10);
+    control();
+}
+
+//------------control the app
+function control() {
+    switch (action) {
+        case 0:
+            start();
+            break;
+        case 1:
+            addTodo();
+            break;
+    }
+}
+
+// ------------add new todo
+function addTodo() {
+    const newTodo = prompt("Add new todo");
+    todos.push(newTodo);
+    let next = confirm('Continue') ? addTodo() : start();
+}
+
+
+
+
+
+
+
+
