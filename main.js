b43669c66f4ca5f7e77164f975518a0f5446618f
Aimekram <janina.byszewska@gmail.com>
2019-11-17 22:11:14 +0100

    Add setDone method to todos

--- main.js
+++ main.js
@@ -50,10 +50,15 @@ class TodoList {
     }
 
     editTodo() {
-        let editIndex = prompt(`Enter the number of the todo you want to edit\n ${printTodos}`)-1;
+        let editIndex = prompt(`Enter the number of the todo you want to edit\n ${this.printTodos}`)-1;
         this.todos[editIndex] = prompt(`Write the new version of this task"\n${this.todos[editIndex]}`);
-        
         confirm('Edit more?') ? this.editTodo() : start();
+    }
+
+    setDone() {
+        let doneIndex = prompt(`Enter the number of the todo you want to change status\n ${this.printTodos}`)-1;
+        this.todos[doneIndex].done = !this.todos[doneIndex].done
+        confirm('Edit more todos?') ? this.setDone() : start();
     }
 }
 
@@ -77,13 +82,16 @@ function control() {
         case 3:
             myList.editTodo();
             break;
+        case 4:
+            myList.setDone();
+            break;
     }
 }
 
 // ------------ start panel
-function start(list) {
-    list.logTodos()
-    action = parseInt(prompt('Hello, you entered a todoapp - write a number to choose action\n1 to add a new todo\n2 to delete a todo\n3 to edit a todo'), 10);
+function start() {
+    myList.logTodos()
+    action = parseInt(prompt('Hello, you entered a todoapp - write a number to choose action\n1 to add a new task\n2 to delete a task\n3 to edit a task\n4 to set done status of a given task to true'), 10);
     control(myList);
 }
 
